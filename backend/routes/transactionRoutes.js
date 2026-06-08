af0d0826510c03a8250bde70a9ab9e31a5e341f9
isaiasmoraes694 <isaiasmoraes694@gmail.com>
2026-06-08 06:28:06 -0300

    Add files via upload

---
+++ backend/routes/transactionRoutes.js
@@ -0,0 +1,11 @@
+const express               = require('express');
+const TransactionController = require('../controllers/transactionController');
+const authMiddleware        = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.post('/transacoes/gasto',   authMiddleware, TransactionController.salvarGasto);
+router.post('/transacoes/receita', authMiddleware, TransactionController.salvarReceita);
+router.get('/transacoes',          authMiddleware, TransactionController.listar);
+
+module.exports = router;
