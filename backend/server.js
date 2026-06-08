c42b9ca75c42f8a54a729d4323b20a1c0feb2bd0
isaiasmoraes694 <isaiasmoraes694@gmail.com>
2026-06-08 06:26:34 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-const userRoutes            = require('./routes/userRoutes');
-const authRoutes            = require('./routes/authRoutes');
-const gamificationRoutes    = require('./routes/gamificationRoutes'); // 🆕
+const userRoutes          = require('./routes/userRoutes');
+const authRoutes          = require('./routes/authRoutes');
+const gamificationRoutes  = require('./routes/gamificationRoutes');
+const transactionRoutes   = require('./routes/transactionRoutes'); 
 
 app.use('/api',  userRoutes);
 app.use('/auth', authRoutes);
-app.use('/api',  gamificationRoutes); // 🆕 monta: /api/xp, /api/xp/ganhar, /api/xp/historico
+app.use('/api',  gamificationRoutes);
+app.use('/api',  transactionRoutes); 
 
 app.listen(PORT, () => {
     console.log(`Servidor PiggyMe rodando na porta ${PORT}`);
